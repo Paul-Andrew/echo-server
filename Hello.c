f19a569c10f9552e3ad199eaf4e37d62ed8e4662
Paul-Andrew <paul.andrew.ferguson@gmail.com>
2014-06-12 19:13:41 -0500

    Hello.c
    Prints "Hello, World" to the command line when called.

---
+++ Hello.c
@@ -0,0 +1,21 @@
+/*This file is part of Echo-Server.
+
+Copyright (c) 2014 Paul Ferguson
+
+Echo-Server is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Echo-Server is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Echo-Server. If not, see http://www.gnu.org/licenses/.
+*/
+
+/*
+Hello, World By Paul Ferguson 
+*/
+#include <stdio.h> //Allows IO
+int main()
+{
+    printf( "Hello, World!\n" );//Prints "Hello, World!"
+    getchar();
+    return 0;//Ends Main function
+}
